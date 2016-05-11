46b462c0f1b12030ef27a3f3e3278c82243d8b4e
Ander Orbegozo <9and3r@gmail.com>
2016-05-11 17:01:52 +0200

    Mas o menos bukatuta

--- Kodea/server.R
+++ Kodea/server.R
@@ -32,8 +32,8 @@ shinyServer(function(input, output) {
 	data <- NULL
     # Check for changes in the loaded files
 	if (!is.null(input$data_loader) & (is.null(rvalues$file_names) ||
-		length(input$data_loader$names)!=length(rvalues$file_names) || 
-		all(sort(input$data_loader$names)==sort(rvalues$file_names)))) {
+		length(input$data_loader$name)!=length(rvalues$file_names) || 
+		all(sort(input$data_loader$name)==sort(rvalues$file_names)))) {
 	  
 	  #Jakiteko datuak kargatzen dagoen
 	  print("Datuak kargatzen... Behin bakarrik agertu behar naiz :)")
@@ -65,41 +65,53 @@ shinyServer(function(input, output) {
   
   # OUTPUTS ------------------------------------------------------------
   
+  # DropDown list eta ezabatu botoia
+  output$output_ui_panel <- renderUI({
+    raw.data <- getRawData()
+    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
+    if (length(fitxategiak) > 0){
+      tagList(selectInput("dataset", "Data set", fitxategiak),
+              actionButton("ezabatu", "Ezabatu"))
+    }
+  })
+  
+  # Ezabatzeko botoia sakatzen denean
+  observeEvent(input$ezabatu, {
+    raw.data <- getRawData()
+    if (!is.null(raw.data$raw.data) && !is.null(raw.data$fitxategiak) && !is.null(input$dataset)){
+      raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$dataset]
+      pos <- grep(input$dataset, colnames(raw.data$raw.data))
+      if (length(pos) > 0 && pos > 0){
+        raw.data$raw.data <- raw.data$raw.data[-pos]
+      }
+    }
+  })
+  
+  daturikEz <- function(){
+    plot(c(1,2,3,4), c(1,2,3,4))
+    text(2, 2, "Ez dago daturik")
+  }
   
   
   ###########
   # Panel 1 #
   ###########
   
-  output$panel1_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    if (length(fitxategiak) > 0){
-        selectInput("panel1_dataset", "Data set", fitxategiak)
-    }
-  })
+  
   
   output$panel1_irudia <- renderPlot({
     raw.data <- getRawData()
-    if (!is.null(input$panel1_dataset) && !is.null(raw.data$raw.data)){
-        pos <- grep(input$panel1_dataset, colnames(raw.data$raw.data))
+    if (!is.null(input$dataset) && !is.null(raw.data$raw.data)){
+        pos <- grep(input$dataset, colnames(raw.data$raw.data))
         if (length(pos) > 0 && pos > 0){
           image(raw.data$raw.data[,pos])
         }
+    }else{
+      daturikEz()
     }
   })
   
-  observeEvent(input$ezabatu, {
-    raw.data <- getRawData()
-    if (!is.null(raw.data$raw.data) && !is.null(raw.data$fitxategiak) && !is.null(input$panel1_dataset)){
-      raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel1_dataset]
-      pos <- grep(input$panel1_dataset, colnames(raw.data$raw.data))
-      if (length(pos) > 0 && pos > 0){
-        raw.data$raw.data <- raw.data$raw.data[-pos]
-      }
-    }
-    
-  })
+  
   
   ###########
   # Panel 2 #
@@ -107,89 +119,57 @@ shinyServer(function(input, output) {
   
   output$panel2_boxplot <- renderPlot({
     raw.data <- getRawData()
-    res <- NULL
-    if(!is.null(rvalues$directory)){
+    if (!is.null(raw.data) && !is.null(raw.data$raw.data)){
       boxplot(raw.data$raw.data, col=raw.data$raw.data@phenoData@data$Type)#     boxplot(raw.data$raw.data)
     }
   })
   
-  output$panel2_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    selectInput("panel2_dataset", "Data set", fitxategiak)
-  })
-  
-  observeEvent(input$ezabatu2, {
-    raw.data <- getRawData()
-    raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel2_dataset]
-    raw.data$raw.data <- raw.data$raw.data[-grep(input$panel2_dataset, colnames(raw.data$raw.data))]
-  })
-  
-  
-  
-  
   output$panel2_irudia1 <- renderPlot({
     raw.data <- getRawData()
-    res <- NULL
-    if(!is.null(rvalues$directory)){
-      res <- hist(raw.data$raw.data)
+    if (!is.null(raw.data) && !is.null(raw.data$raw.data)){
+      hist(raw.data$raw.data)
     }
-    print(res)
   })
   
   output$panel2_irudia2 <- renderPlot({
     raw.data <- getRawData()
-    res <- NULL
-    if(!is.null(rvalues$directory)){
+    if (!is.null(raw.data) && !is.null(raw.data$raw.data)){
       data.deg <- AffyRNAdeg(raw.data$raw.data)
-      res <- plotAffyRNAdeg(data.deg)
+      plotAffyRNAdeg(data.deg)
     }
-    print(res)
   })
   
   ###########
   # Panel 3 #
   ###########
   
-  output$panel3_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    selectInput("panel3_dataset", "Data set", fitxategiak)
-  })
-  
-  
-  
   output$panel3_irudia <- renderPlot({
     
-    
-    
-    
     raw.data <- getRawData()
+    if (!is.null(input$dataset) && !is.null(raw.data$raw.data)){
     
-    num.arrays <- length(raw.data$raw.data)
+      num.arrays <- length(raw.data$raw.data)
     
-    medians <- sapply(1:num.arrays, 
-                      FUN=function(i) {
-                        return(median(exprs(raw.data$raw.data[,i])))
-                      })
+      medians <- sapply(1:num.arrays, 
+                        FUN=function(i) {
+                          return(median(exprs(raw.data$raw.data[,i])))
+                        })
     
-    id.ref <- order(medians)[num.arrays/2]
+      id.ref <- order(medians)[num.arrays/2]
     
-    plotMA <- function (data, index) {
-      m <- exprs(data[,index]) - exprs(data[,id.ref])
-      a <- (exprs(data[,index]) + exprs(data[,id.ref]))/2
-      ma.plot(a,m,cex=0.75,lwd=3)
+      plotMA <- function (data, index) {
+        m <- exprs(data[,index]) - exprs(data[,id.ref])
+        a <- (exprs(data[,index]) + exprs(data[,id.ref]))/2
+        ma.plot(a,m,cex=0.75,lwd=3)
+      }
+    
+      # Get the data set with the appropriate name
+      #dat <- get(input$dataset)
+      pos <- grep(input$dataset, colnames(raw.data$raw.data))
+      if (length(pos) > 0 && pos > 0){
+        plotMA(raw.data$raw.data, pos)
+      }
     }
-    
-    # Get the data set with the appropriate name
-    #dat <- get(input$dataset)
-    plotMA(raw.data$raw.data, grep(input$panel3_dataset, colnames(raw.data$raw.data)))
-  })
-  
-  observeEvent(input$ezabatu3, {
-    raw.data <- getRawData()
-    raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel3_dataset]
-    raw.data$raw.data <- raw.data$raw.data[-grep(input$panel3_dataset, colnames(raw.data$raw.data))]
   })
   
   ###########
@@ -198,100 +178,71 @@ shinyServer(function(input, output) {
   
   output$panel4_irudia <- renderPlot({
     raw.data <- getRawData()
-    mas5.data <- call.exprs(raw.data$raw.data,"mas5")
-    qcs <- qc(raw.data$raw.data,mas5.data)
-    plot(qcs)
-  })
-  
-  observeEvent(input$ezabatu4, {
-    raw.data <- getRawData()
-    raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel4_dataset]
-    raw.data$raw.data <- raw.data$raw.data[-grep(input$panel4_dataset, colnames(raw.data$raw.data))]
-  })
-  
-  output$panel4_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    selectInput("panel4_dataset", "Data set", fitxategiak)
+    if (!is.null(raw.data$raw.data)){
+      mas5.data <- call.exprs(raw.data$raw.data,"mas5")
+      qcs <- qc(raw.data$raw.data,mas5.data)
+      plot(qcs)
+    }
   })
   
   ###########
   # Panel 5 #
   ###########
   
-  output$panel5_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    selectInput("panel5_dataset", "Data set", fitxategiak)
-  })
-  
-  observeEvent(input$ezabatu5, {
-    raw.data <- getRawData()
-    raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel5_dataset]
-    raw.data$raw.data <- raw.data$raw.data[-grep(input$panel5_dataset, colnames(raw.data$raw.data))]
-  })
-  
   output$panel5_irudia1 <- renderPlot({
     raw.data <- getRawData()
-    # Normalization
-    rma.data <- call.exprs(raw.data$raw.data,"rma")
+    
+    if (!is.null(raw.data$raw.data)){
+    
+      # Normalization
+      rma.data <- call.exprs(raw.data$raw.data,"rma")
     
     
-    # Boxplot and distribution after normalization
-    boxplot(exprs(rma.data), col=raw.data$raw.data@phenoData@data$Type)
+      # Boxplot and distribution after normalization
+      boxplot(exprs(rma.data), col=raw.data$raw.data@phenoData@data$Type)
+    }
   })
   
   output$panel5_irudia2 <- renderPlot({
     raw.data <- getRawData()
-    
-    rma.data <- call.exprs(raw.data$raw.data,"rma")
-    plot(density(exprs(rma.data[,1]),col=raw.data$raw.data@phenoData@data$Type[1]))
-    sapply(2:length(raw.data$raw.data), 
-           FUN=function(i) {
-             lines(density(exprs(rma.data[,i])), col=raw.data$raw.data@phenoData@data$Type[i])
-           })
+    if (!is.null(raw.data$raw.data)){
+      rma.data <- call.exprs(raw.data$raw.data,"rma")
+      plot(density(exprs(rma.data[,1]),col=raw.data$raw.data@phenoData@data$Type[1]))
+      sapply(2:length(raw.data$raw.data), 
+            FUN=function(i) {
+              lines(density(exprs(rma.data[,i])), col=raw.data$raw.data@phenoData@data$Type[i])
+            })
+    }
   })
   
   ###########
   # Panel 6 #
   ###########
   
-  
-  output$panel6_output <- renderUI({
-    raw.data <- getRawData()
-    fitxategiak <-  raw.data$fitxategiak[grep("*.CEL", raw.data$fitxategiak)]
-    selectInput("panel6_dataset", "Data set", fitxategiak)
-  })
-  
   output$panel6_irudia <- renderPlot({
-    
     raw.data <- getRawData()
-    rma.data <- call.exprs(raw.data$raw.data,"rma")
+    if (!is.null(input$dataset) && !is.null(raw.data$raw.data)){
+      raw.data <- getRawData()
+      rma.data <- call.exprs(raw.data$raw.data,"rma")
     
-    medians <- sapply(1:length(raw.data$raw.data), 
-                      FUN=function(i) {
-                        return(median(exprs(raw.data$raw.data[,i])))
-                      })
+      medians <- sapply(1:length(raw.data$raw.data), 
+                        FUN=function(i) {
+                          return(median(exprs(raw.data$raw.data[,i])))
+                        })
     
-    id.ref <- order(medians)[length(raw.data$raw.data)/2]
+      id.ref <- order(medians)[length(raw.data$raw.data)/2]
     
-    plotMA <- function (data, index) { 
-      m <- exprs(data[,index]) - exprs(data[,id.ref])
-      a <- (exprs(data[,index]) + exprs(data[,id.ref]))/2
-      ma.plot(a,m,cex=0.75,lwd=3)
+      plotMA <- function (data, index) { 
+        m <- exprs(data[,index]) - exprs(data[,id.ref])
+        a <- (exprs(data[,index]) + exprs(data[,id.ref]))/2
+        ma.plot(a,m,cex=0.75,lwd=3)
+      }
+    
+      pos <- grep(input$dataset, colnames(raw.data$raw.data))
+      if (length(pos) > 0 && pos > 0){
+        plotMA(rma.data, pos)
+      }
     }
-    
-    
-    plotMA(rma.data, grep(input$panel6_dataset, colnames(raw.data$raw.data)))
   })
-  
-  observeEvent(input$ezabatu6, {
-    raw.data <- getRawData()
-    raw.data$fitxategiak <- raw.data$fitxategiak[raw.data$fitxategiak != input$panel6_dataset]
-    raw.data$raw.data <- raw.data$raw.data[-grep(input$panel6_dataset, colnames(raw.data$raw.data))]
-  })
-  
-  
-
 })
 
--- Kodea/ui.R
+++ Kodea/ui.R
@@ -6,16 +6,21 @@ fileLoader <- fileInput (inputId="data_loader",
 						 label="Upload Files", 
 						 multiple=TRUE)
 
+
+
 shinyUI(fluidPage(
+  title = 'Bioinformatika',
 	header, 
-	fileLoader,
+  wellPanel(
+    fileLoader,
+    uiOutput("output_ui_panel")),
 	  tabsetPanel("true",
-	    tabPanel("kaka", uiOutput("panel1_output"), plotOutput(outputId="panel1_irudia", width="500px"), actionButton("ezabatu", "Ezabatu")),
-	    tabPanel("kaka 2", uiOutput("panel2_output"), actionButton("ezabatu2", "Ezabatu"), plotOutput(outputId="panel2_boxplot", width="500px"), plotOutput(outputId="panel2_irudia1", width="500px"), plotOutput(outputId="panel2_irudia2", width="500px")),
-	    tabPanel("kaka 3", uiOutput("panel3_output"), plotOutput(outputId="panel3_irudia", width="500px"), actionButton("ezabatu3", "Ezabatu")),
-	    tabPanel("kaka 4", uiOutput("panel4_output"), actionButton("ezabatu4", "Ezabatu"), plotOutput(outputId="panel4_irudia", width="500px")),
-	    tabPanel("kaka 5", uiOutput("panel5_output"), actionButton("ezabatu5", "Ezabatu"), plotOutput(outputId="panel5_irudia1", width="500px"), plotOutput(outputId="panel5_irudia2", width="500px")),
-	    tabPanel("kaka 6", uiOutput("panel6_output"), plotOutput(outputId="panel6_irudia", width="500px"), actionButton("ezabatu6", "Ezabatu"))
+	    tabPanel("Irudiaen egiaztapena", plotOutput(outputId="panel1_irudia", width="500px")),
+	    tabPanel("Grafiko esanguratsuak", plotOutput(outputId="panel2_boxplot", width="500px"), plotOutput(outputId="panel2_irudia1", width="500px"), plotOutput(outputId="panel2_irudia2", width="500px")),
+	    tabPanel("Erregresioen grafikoa", plotOutput(outputId="panel3_irudia", width="500px")),
+	    tabPanel("QC estatistikak", plotOutput(outputId="panel4_irudia", width="500px")),
+	    tabPanel("Normalizatutako ondorengo grafikoak", plotOutput(outputId="panel5_irudia1", width="500px"), plotOutput(outputId="panel5_irudia2", width="500px")),
+	    tabPanel("Erregresioak normalizatu ondoren", plotOutput(outputId="panel6_irudia", width="500px"))
 	   )
 ))
 
